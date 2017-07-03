9fedfb7a6da021f7a0651da62cf2b7a2cb3e97b1
larkenx <larkenx@gmail.com>
2017-07-02 23:52:05 -0400

    Automated commit

---
+++ assets/maps/old/test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="compiled_tileset_32x32" tilewidth="32" tileheight="32" tilecount="13080" columns="120">
+ <image source="../../images/DawnLike/Compiled/compiled_tileset_32x32.png" width="3840" height="3488"/>
+</tileset>
