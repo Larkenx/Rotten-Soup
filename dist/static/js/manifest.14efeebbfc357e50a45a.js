0654ac8c5a9c6b169a131dc0fcec594fa94fbd92
larkenx <larkenx@gmail.com>
2017-11-14 15:45:59 -0500

    building for heroku

--- dist/static/js/manifest.14efeebbfc357e50a45a.js
+++ dist/static/js/manifest.14efeebbfc357e50a45a.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "static/js/" + chunkId + "." + {"0":"9fc61c5e6459f2691927","1":"01751459f0d211717dd1"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "static/js/" + chunkId + "." + {"0":"d26b2064c6834d93bbac","1":"01751459f0d211717dd1"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
@@ -151,4 +151,4 @@
 /******/ })
 /************************************************************************/
 /******/ ([]);
-//# sourceMappingURL=manifest.eaa39cab719c9f0cf62a.js.map
+//# sourceMappingURL=manifest.14efeebbfc357e50a45a.js.map
