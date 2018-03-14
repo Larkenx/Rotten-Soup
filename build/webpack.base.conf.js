b2467f2090142fff106dbaefc1c3bb0342e35d41
larkenx <larkenx@gmail.com>
2018-03-14 17:22:33 -0400

    updated webpack configuration, mostly for refresh browser plugin

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,16 +1,15 @@
-var path = require('path')
-var fs = require('fs')
-var utils = require('./utils')
-var config = require('../config')
-var vueLoaderConfig = require('./vue-loader.conf')
-var webpack = require('webpack')
-// const RefreshBrowserPlugin = require('refresh-browser-webpack-plugin')
+'use strict'
+const path = require('path')
+const utils = require('./utils')
+const config = require('../config')
+const vueLoaderConfig = require('./vue-loader.conf')
 
 function resolve(dir) {
 	return path.join(__dirname, '..', dir)
 }
 
 module.exports = {
+	context: path.resolve(__dirname, '../'),
 	entry: {
 		app: ['refresh-browser-webpack-plugin/client', './src/main.js']
 	},
@@ -24,9 +23,8 @@ module.exports = {
 		alias: {
 			vue$: 'vue/dist/vue.esm.js',
 			'@': resolve('src'),
-			'#': resolve('src/assets/js/game/')
-		},
-		symlinks: false
+			'#': resolve('src/assets/js/game')
+		}
 	},
 	module: {
 		rules: [
@@ -38,14 +36,10 @@ module.exports = {
 			{
 				test: /\.js$/,
 				loader: 'babel-loader',
+				include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')],
 				query: {
-					presets: ['babel-preset-stage-0']
-				},
-				exclude: /node_modules/
-			},
-			{
-				test: /\.json$/,
-				loader: 'json-loader'
+					plugins: ['transform-object-rest-spread']
+				}
 			},
 			{
 				test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
@@ -73,6 +67,16 @@ module.exports = {
 			}
 		]
 	},
-
-	plugins: []
+	node: {
+		// prevent webpack from injecting useless setImmediate polyfill because Vue
+		// source contains it (although only uses it if it's native).
+		setImmediate: false,
+		// prevent webpack from injecting mocks to Node native modules
+		// that does not make sense for the client
+		dgram: 'empty',
+		fs: 'empty',
+		net: 'empty',
+		tls: 'empty',
+		child_process: 'empty'
+	}
 }
