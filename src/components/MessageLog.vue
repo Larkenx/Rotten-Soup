4ab537a8aa5f7100830a07b34f5f69001d0b953b
Steven Myers <larkenx@gmail.com>
2018-05-15 23:43:57 -0400

    added up and down arrows to scroll messages

--- src/components/MessageLog.vue
+++ src/components/MessageLog.vue
@@ -1,15 +1,39 @@
 <template>
-    <v-layout row style="padding: 5px 0 0 0; margin: 0px">
-        <v-list id="console"
-                style="overflow:hidden; padding-top: 8px; position: relative;">
-            <li v-for="(message, index) in getMessages()" v-bind:key="index">
-                <p><b v-bind:style="{color : message[1]}">{{message[0]}}</b></p>
-            </li>
-            <!-- Temporary log item for displaying info that isn't stored (examine text, or "this is blocked")  -->
-            <li v-for="(message, index) in getTempMessages()">
-                <p><b v-bind:style="{color : message[1]}">{{message[0]}}</b> </p>
-            </li>
+    <v-layout id="console_container">
+      <v-flex column align-start>
+        <v-list id="console">
+          <li class="ml-2" v-for="(message, index) in getMessages()" v-bind:key="index">
+            <p><b v-bind:style="{color : message[1]}">{{message[0]}}</b></p>
+          </li>
+          <!-- Temporary log item for displaying info that isn't stored (examine text, or "this is blocked")  -->
+          <li class="ml-2" v-for="(message, index) in getTempMessages()">
+            <p><b v-bind:style="{color : message[1]}">{{message[0]}}</b> </p>
+          </li>
         </v-list>
+      </v-flex>
+      <v-flex id="scroll_controls" column>
+        <v-layout>
+          <v-flex xs12 class="text-xs-right">
+            <v-icon class="scroll_controls" v-on:click="up()" small>fa-angle-up</v-icon>
+          </v-flex>
+        </v-layout>
+        <v-layout style="height: 50%"></v-layout>
+        <v-layout>
+          <v-flex xs12 class="text-xs-right">
+            <v-icon class="scroll_controls" v-on:click="down()" small>fa-angle-down</v-icon>
+          </v-flex>
+        </v-layout>
+        <v-layout>
+          <v-flex xs12 class="text-xs-right">
+              <v-icon class="scroll_controls" v-on:click="resetOffset()" small>fa-angle-double-down</v-icon>
+          </v-flex>
+        </v-layout>
+        <!-- <v-layout fill-height>
+          <v-flex xs12 class="text-xs-right mr-2">
+            <v-icon small>description</v-icon>
+          </v-flex>
+        </v-layout> -->
+      </v-flex>
     </v-layout>
 </template>
 
@@ -21,7 +45,8 @@ export default {
 	data() {
 		return {
 			messages: Game.messageHistory,
-			tempMessages: Game.tempMessages
+			tempMessages: Game.tempMessages,
+			offset: 0
 		}
 	},
 	methods: {
@@ -29,25 +54,56 @@ export default {
 			return this.tempMessages
 		},
 		getMessages() {
-			return this.messages.slice(-10 + this.getTempMessages().length)
+			return this.messages.slice(-10 + -this.offset + this.getTempMessages().length)
+		},
+		resetOffset() {
+			this.offset = 0
+		},
+		down() {
+			if (this.offset > 0) this.offset--
+		},
+		up() {
+			if (this.offset < this.messages.length - 10) this.offset++
 		}
 	}
 }
 </script>
 
 <style>
+#console_container {
+	border: 2px solid #4f4f4f;
+	background-color: #1e1f1f;
+	border-radius: 4px;
+	padding: 5px 0 0 0;
+	margin: 0px;
+}
 
-    #console {
-        font-size: 14px;
-        font-style: normal;
-        font-weight: normal;
-        background-color: black;
-        height: 175px;
-        line-height: 0;
-    }
+#console {
+	font-size: 14px;
+	font-style: normal;
+	font-weight: normal;
+	height: 175px;
+	line-height: 0;
+	overflow: hidden;
+	padding-top: 8px;
+	position: relative;
+	background-color: inherit;
+}
 
-    ul {
-        padding: 0;
-        list-style-type: none;
-    }
+.scroll_controls {
+	cursor: pointer;
+	padding: 3px;
+	margin-right: 5px;
+}
+
+.scroll_controls:hover {
+	cursor: pointer;
+	border-radius: 4px;
+	background-color: #4a5d69;
+}
+
+ul {
+	padding: 0;
+	list-style-type: none;
+}
 </style>
