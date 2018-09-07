7e01ba776f1998faa4a9edb56bd296a32a9f47be
Steven Myers <larkenx@gmail.com>
2018-09-07 13:55:46 -0400

    moving the old versions to their own folder

--- server.js
+++ server.js
@@ -2,14 +2,10 @@ var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 app = express()
-// app.get('/', (req, res) => {
-// 	res.sendFile(__dirname + '/dist/index.html')
-// })
-
 app.use('/', express.static(path.join(__dirname, 'dist')))
-app.use('/v1', express.static(path.join(__dirname, 'v1')))
-app.use('/v2', express.static(path.join(__dirname, 'v2')))
-app.use('/v3', express.static(path.join(__dirname, 'v3')))
+app.use('/v1', express.static(path.join(__dirname, 'legacy/v1')))
+app.use('/v2', express.static(path.join(__dirname, 'legacy/v2')))
+app.use('/v3', express.static(path.join(__dirname, 'legacy/v3')))
 
 var port = process.env.PORT || 5000
 app.listen(port)
