0f7e151901f8b6234019b86d15d418380a4ccd8f
Steven Myers <larkenx@gmail.com>
2018-12-23 18:21:03 -0500

    updating to vue-cli 3 (moved a lot of files around)

--- src/assets/js/game/ResourceLoader.js
+++ src/assets/js/game/ResourceLoader.js
@@ -3,36 +3,36 @@ import * as PIXI from 'pixi.js'
 export default function loadResources(callbacks) {
 	/* Images & Texture Atlas */
 	const spritesheet = {
-		url: 'static/images/compiled_tileset_32x32.png',
+		url: 'images/compiled_tileset_32x32.png',
 		name: 'spritesheet'
 	}
 	const textureAtlas = {
-		url: 'static/compiled_dawnlike.json',
+		url: 'tileset/compiled_dawnlike.json',
 		name: 'textureAtlas'
 	}
 	/* Maps */
 	const mulberryTown = {
-		url: 'static/maps/mulberryTown.json',
+		url: 'maps/mulberryTown.json',
 		name: 'mulberryTown'
 	}
 
 	const mulberryForest = {
-		url: 'static/maps/mulberryForest.json',
+		url: 'maps/mulberryForest.json',
 		name: 'mulberryForest'
 	}
 
 	const mulberryGraveyard = {
-		url: 'static/maps/mulberryGraveyard.json',
+		url: 'maps/mulberryGraveyard.json',
 		name: 'mulberryGraveyard'
 	}
 
 	const lichLair = {
-		url: 'static/maps/lichLair.json',
+		url: 'maps/lichLair.json',
 		name: 'lichLair'
 	}
 
 	const lootGoblinLair = {
-		url: 'static/maps/lootGoblinLair.json',
+		url: 'maps/lootGoblinLair.json',
 		name: 'lootGoblinLair'
 	}
 
@@ -40,7 +40,7 @@ export default function loadResources(callbacks) {
 	for (let i = 1; i <= 35; i++) {
 		prefabs.push({
 			name: `ruins-${i}`,
-			url: `static/maps/prefabs/ruins-${i}.json`
+			url: `maps/prefabs/ruins-${i}.json`
 		})
 	}
 
